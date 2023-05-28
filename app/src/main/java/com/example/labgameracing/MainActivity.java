9962132f3aa20d5719ded16e38d211f42b3c21db
Võ Ngọc Trúc Lam <v.trclam@gmail.com>
2023-05-28 17:43:56 +0700

    [Lam][Done Review HA branch] (#3)
    * Thong edit layout
    
    * Bao Ngoc edit layout
    
    * Lam completed Play a run and Reset
    
    * Thuong Add animation and audio
    
    * HoangAnh Update MainActivity.java - Adding Bet Money Calculator function
    
    * Bao Ngoc update layout
    
    * [Lam][Update .gitignore]
    
    * [Lam][Edit README.md]
    
    * Lam Delete .name
    
    * Lam Delete compiler.xml
    
    * [Lam][Merge with lastest branch]
    
    * [Lam][Fix HA bugs]
    
    ---------
    
    Co-authored-by: tranlebaongoc1512 <tranlebaongoc1512@gmail.com>
    Co-authored-by: trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
    Co-authored-by: giathuong172 <thuongmc2k2@gmail.com>
    Co-authored-by: danghoanganh36@gmail.com <danghoanganh36@gmail.com>

--- app/src/main/java/com/example/labgameracing/MainActivity.java
+++ app/src/main/java/com/example/labgameracing/MainActivity.java
@@ -11,9 +11,10 @@ import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
@@ -25,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
     SeekBar sbPlayer3;
     Button btnStart;
     Button btnReset;
-
+    CheckBox checkboxPlayer1, checkboxPlayer2, checkboxPlayer3;
+    EditText etPayNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
         btnStart = findViewById(R.id.btnStart);
         btnReset = findViewById(R.id.btnReset);
         int color = ContextCompat.getColor(this,R.color.orange);
+        checkboxPlayer1 = findViewById(R.id.checkboxPlayer1);
+        checkboxPlayer2 = findViewById(R.id.checkboxPlayer2);
+        checkboxPlayer3 = findViewById(R.id.checkboxPlayer3);
+        etPayNumber = findViewById(R.id.etPayNumber);
+
         // Logic code
         tvCurrentAmount.setText(INIT_AMOUNT+"");
         MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.music);
@@ -57,24 +64,28 @@ public class MainActivity extends AppCompatActivity {
                 int progress3 = 0;
                 int height = 100;
                 int thumbWidth = 200;
+                final int minBound = 70;
+                final int maxBound = 95;
+                final int animationDuration = 12000;
                 boolean flag = true;
                 while (flag) {
-                    progress1 = 70 + random.nextInt(30) + 1;
-                    progress2 = 70 + random.nextInt(30) + 1;
-                    progress3 = 70 + random.nextInt(30) + 1;
-                    if (progress1 == 100 || progress2 == 100 || progress3 == 100) {
+                    progress1 = minBound + random.nextInt(maxBound-minBound) + 1;
+                    progress2 = minBound + random.nextInt(maxBound-minBound) + 1;
+                    progress3 = minBound + random.nextInt(maxBound-minBound) + 1;
+                    if (progress1 == maxBound || progress2 == maxBound || progress3 == maxBound) {
                         flag = false;
                     }
                 }
-                ObjectAnimator animation = ObjectAnimator.ofInt(sbPlayer1, "progress", 0, progress1);
-                animation.setDuration(12000);
+                ObjectAnimator animation = ObjectAnimator.ofInt(sbPlayer1, "progress", 5, progress1);
+                animation.setDuration(animationDuration);
                 animation.start();
-                ObjectAnimator animation1 = ObjectAnimator.ofInt(sbPlayer2, "progress", 0, progress2);
-                animation1.setDuration(12000);
+                ObjectAnimator animation1 = ObjectAnimator.ofInt(sbPlayer2, "progress", 5, progress2);
+                animation1.setDuration(animationDuration);
                 animation1.start();
-                ObjectAnimator animation2 = ObjectAnimator.ofInt(sbPlayer3, "progress", 0, progress3);
-                animation2.setDuration(12000);
+                ObjectAnimator animation2 = ObjectAnimator.ofInt(sbPlayer3, "progress", 5, progress3);
+                animation2.setDuration(animationDuration);
                 animation2.start();
+                tvCurrentAmount.setText(calculateBetMoney(progress1, progress2, progress3)+"");
             }
         });
     }
@@ -82,10 +93,42 @@ public class MainActivity extends AppCompatActivity {
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sbPlayer1.setProgress(0, true);
-                sbPlayer2.setProgress(0, true);
-                sbPlayer3.setProgress(0, true);
+                sbPlayer1.setProgress(5, true);
+                sbPlayer2.setProgress(5, true);
+                sbPlayer3.setProgress(5, true);
             }
         });
+    }
+    int calculateBetMoney(int progress1, int progress2, int progress3) {
+        boolean checkedPlayer1 = checkboxPlayer1.isChecked();
+        boolean checkedPlayer2 = checkboxPlayer2.isChecked();
+        boolean checkedPlayer3 = checkboxPlayer3.isChecked();
+        int currentMoney = Integer.parseInt(tvCurrentAmount.getText().toString());
+        if (!etPayNumber.getText().toString().equals("")) {
+            int betMoney = Integer.parseInt(etPayNumber.getText().toString());
+
+            if (checkedPlayer1) {
+                if (progress1 < progress2 || progress1 < progress3) {
+                    currentMoney = currentMoney - betMoney;
+                } else if (progress1 >= progress2 && progress1 >= progress3) {
+                    currentMoney = currentMoney + betMoney;
+                }
+            }
+            if (checkedPlayer2) {
+                if (progress2 < progress1 || progress2 < progress3) {
+                    currentMoney = currentMoney - betMoney;
+                } else if (progress2 >= progress1 && progress2 >= progress3) {
+                    currentMoney = currentMoney + betMoney;
+                }
+            }
+            if (checkedPlayer3) {
+                if (progress3 < progress1 || progress3 < progress2) {
+                    currentMoney = currentMoney - betMoney;
+                } else if (progress3 >= progress1 && progress3 >= progress2) {
+                    currentMoney = currentMoney + betMoney;
+                }
+            }
+        }
+        return currentMoney;
     }
 }
